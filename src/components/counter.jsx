ef8a6ee9aa149b4ef39fa548a482a3899d43ac3c
Zeeshan Khan <zeeshank1@gmail.com>
2021-01-19 00:23:37 +0530

    second commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+
+class Counter extends Component {
+    // state = { 
+    //     count : 0,
+    //     tags:['tag1','tag2','tag3']
+    //     // ,imageURL : 'https://picsum.photos/200'
+    // };
+    
+    //State after Counters.jsx and props introduction
+    state = { 
+        //Changes after sending complete counter object 
+        count : this.props.counter.value, //count : this.props.value,//
+        tags:['tag1','tag2','tag3']
+    };
+
+    //State after Counters.jsx and props introduction
+    constructor(props){
+        super(props);
+        console.log('This :',props);
+        console.log('This :',this);
+        this.props = props;
+        //1. Old Way to bind this reference in case if it is missing : 
+        //this.handleIncrement() = this.handleIncrement().bind(this);
+
+        //2. Second way : to use arrow functions
+        
+    }
+//
+    // constructor(){
+    //     super();
+    //     console.log('This :',this);
+    //     //1. Old Way to bind this reference in case if it is missing : 
+    //     //this.handleIncrement() = this.handleIncrement().bind(this);
+
+    //     //2. Second way : to use arrow functions
+        
+    // }
+
+    styles = {
+        fontWeight : 'bold',
+        fontSize : 30// react will convert '10px'
+    };
+    
+    render() {
+        //This is after 2nd way in Counters.jsx
+        console.log('Props : ',this.props);
+        
+        // Our Virtual DOM is a tree with React Element on the top and children under it. react put virtual DOM and DOM, compare them, it will update the DOM with the change element only
+        let classes = this.formatClasses();
+
+        //INFO : $r is the react element representation
+        return (
+            <React.Fragment>
+                {/* <img src={this.state.imageURL} alt=''></img> */}
+                {/* <span style={ this.styles } className='badge badge-primary m-2'>{this.formatCount()}</span> */}
+                {/* <span style={{ fontSize:20 }} className='badge badge-warning m-2'>{this.formatCount()}</span> */}
+                {/*we do not have loops like angular in JSX or If-Else statement, becoz JSX is not a templating engine*/}
+                {/* {this.props.children}
+                {this.renderTags()}
+                {this.state.tags.length > 0 && 'Please add more elements'} */}
+                {/*In javascript you can apply logical and operator between non boolean values*/}
+                <br/>
+                <span className={this.formatClasses()}>{this.formatCount()}</span>
+                <button onClick={() => this.handleIncrement()} className='btn btn-secondary btn-sm'>Increment</button>
+                <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+            </React.Fragment>
+        );
+    }
+
+    //2ND way
+    // handleIncrement(){
+    //     console.log('Incremental Clicked',this);
+    // }
+
+    //or 1st way
+    handleIncrement = () => {
+        //this.props.value = 100;//Props are read-only you cannot assign a new value to it
+        //this.state.count++;//its incrementing but react is not aware of it. to have this in React we have to use Component class's setState(), this is done automatically in angular because in angular all browser event are monkey patched, for react we have to explictly tell react what is changed.
+        this.setState({ count: this.state.count+1 });//React will schedule a call to render() method in future and this will be an asyc call.
+
+        console.log('Incremental Clicked :  ',this);
+    }
+
+    doHandleIncrement = () => {
+        this.handleIncrement()
+    }
+
+    renderTags(){
+        if(this.state.tags.length === 0) {
+            return <p>There are no elements</p>;
+        }
+        return <ul> { this.state.tags.map(tagVar =>  <li key={tagVar}>{tagVar}</li>) }</ul>
+    }
+
+    formatClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const {count} = this.state; 
+        return count === 0 ? 'Zero' : count; 
+        //return this.state.count === 0 ? 'Zero' : this.state.count;
+    }
+    // render() { 
+    //     return (  
+    //     <React.Fragment>
+    //         <h1>Hello world from Component</h1>
+    //         <button>Increment</button>
+    //     </React.Fragment> 
+    //     ); 
+    // }
+}
+
+export default Counter;
---
+++ src/components/counters.jsx
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import Counter from './counter'
+
+class Counters extends Component {
+    state = { 
+        counters : [
+            {id:1,value:4,selected:true},
+            {id:2,value:0,selected:true},
+            {id:3,value:0,selected:true},
+            {id:4,value:0,selected:true}
+        ]
+    }
+
+    //1st way to loop
+    // render() { 
+    //     return ( 
+    //         <div>
+    //             <Counter />
+    //             <Counter />
+    //             <Counter />
+    //             <Counter />
+    //         </div>
+    //      );
+    // }
+
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id!==counterId);
+        console.log(">>>> ",counters);
+        this.setState({counters}); 
+        //console.log("Event handler called!",counterId);
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        } );
+        this.setState({counters});
+    };
+
+    //2nd way to loop with state
+    // render() { 
+    //     return ( 
+    //         <div>
+    //             {this.state.counters.map(counter => 
+    //                 <Counter key={counter.id} id={counter.id} onDelete={()=>this.handleDelete(counter.id)} value={counter.value} selected={counter.selected}>
+    //                     <h4>Title</h4>
+    //                 </Counter>
+    //                 )
+    //             }
+                
+    //         </div>
+    //      );
+    // }
+
+    //3rd way to send only single prop as counter
+    render() { 
+        return ( 
+            <div>
+                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
+                {this.state.counters.map(counter => 
+                    <Counter key={counter.id} onDelete={()=>this.handleDelete(counter.id)} counter={counter}>
+                        <h4>Title</h4>
+                    </Counter>
+                    )
+                }
+                
+            </div>
+         );
+    }
+}
+ 
+export default Counters;
