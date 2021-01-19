a90d1b748c589a886cadb828b45812353281d142
Zeeshan Khan <zeeshank1@gmail.com>
2021-01-19 15:33:52 +0530

    NavBar with Total item count

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,8 @@ class NavBar extends Component {
         return (           
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
-                    <span className="navbar-brand mb-0 h1">Navbar</span>
+                    <span className="navbar-brand mb-0 h1">Navbar
+                    <span className="badge badge-pill badge-secondary m-2">{this.props.totalCounter}</span></span>
                 </div>
             </nav>
          );
