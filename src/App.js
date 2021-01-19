345e1a3cd430afa446a49278f751735f78c9e289
Zeeshan Khan <zeeshank1@gmail.com>
2021-01-19 15:21:46 +0530

    Uplifting state to parent component-1

--- src/App.js
+++ src/App.js
@@ -5,16 +5,65 @@ import Counters from './components/counters';
 import React,{Component} from 'react';
 
 class App extends Component {
-  render(){
-    return (
-      <div>
-        <NavBar />
-        <main className="container">
-          <Counters />
-        </main>
-      </div>
-    );
-  }
+    state = { 
+        counters : [
+            {id:1,value:4,selected:true},
+            {id:2,value:0,selected:true},
+            {id:3,value:0,selected:true},
+            {id:4,value:0,selected:true}
+        ]
+    }
+
+    //1st way to loop
+    // render() { 
+    //     return ( 
+    //         <div>
+    //             <Counter />
+    //             <Counter />
+    //             <Counter />
+    //             <Counter />
+    //         </div>
+    //      );
+    // }
+
+    handleIncrement= (counterId) => {
+        const counters = [...this.state.counters];
+        counters[counterId-1].value++;//This is wrong in react - directly modifing the state 
+        //console.log("on increment called",counterId," dsdsdsds    ",counters);
+        // console.log(this.state.counters[0]);
+        this.setState({counters});
+    }
+
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id!==counterId);
+        console.log(">>>> ",counters);
+        this.setState({counters}); 
+        //console.log("Event handler called!",counterId);
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        } );
+        this.setState({counters});
+    };
+
+    render(){
+        return (
+        <div>
+            <NavBar />
+            <main className="container">
+            <Counters 
+                counters={this.state.counters}
+                onReset={this.handleReset}
+                onIncrement={this.handleIncrement}
+                onDelete={this.handleDelete}
+            />
+            </main>
+        </div>
+        );
+    }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,49 +2,7 @@ import React, { Component } from 'react'
 import Counter from './counter'
 
 class Counters extends Component {
-    state = { 
-        counters : [
-            {id:1,value:4,selected:true},
-            {id:2,value:0,selected:true},
-            {id:3,value:0,selected:true},
-            {id:4,value:0,selected:true}
-        ]
-    }
 
-    //1st way to loop
-    // render() { 
-    //     return ( 
-    //         <div>
-    //             <Counter />
-    //             <Counter />
-    //             <Counter />
-    //             <Counter />
-    //         </div>
-    //      );
-    // }
-
-    handleIncrement= (counterId) => {
-        const counters = [...this.state.counters];
-        counters[counterId-1].value++;//This is wrong in react - directly modifing the state 
-        //console.log("on increment called",counterId," dsdsdsds    ",counters);
-        // console.log(this.state.counters[0]);
-        this.setState({counters});
-    }
-
-    handleDelete = (counterId) => {
-        const counters = this.state.counters.filter(c => c.id!==counterId);
-        console.log(">>>> ",counters);
-        this.setState({counters}); 
-        //console.log("Event handler called!",counterId);
-    }
-
-    handleReset = () => {
-        const counters = this.state.counters.map(c => {
-            c.value = 0;
-            return c;
-        } );
-        this.setState({counters});
-    };
 
     //2nd way to loop with state
     // render() { 
@@ -65,12 +23,12 @@ class Counters extends Component {
     render() { 
         return ( 
             <div>
-                <button onClick={this.handleReset} className="btn btn-primary btn-sm m-2">Reset</button>
-                {this.state.counters.map(counter => 
+                <button onClick={this.props.onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+                {this.props.counters.map(counter => 
                     <Counter 
                         key={counter.id} 
-                        onDelete={ () => this.handleDelete(counter.id)} 
-                        onIncrement={() => this.handleIncrement(counter.id)}
+                        onDelete={ () => this.props.onDelete(counter.id)} 
+                        onIncrement={() => this.props.onIncrement(counter.id)}
                         counter={counter}>
                         <h4>Title</h4>
                     </Counter>
